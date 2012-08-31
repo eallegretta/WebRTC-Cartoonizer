480d53fb039082ac3701ad526ac4fb5aeb83e5c1
Paleta <epaleta@hotmail.com>
2012-08-30 23:38:07 -0300

    Another test

--- cartoonizer.js
+++ cartoonizer.js
@@ -23,7 +23,17 @@ var Cartoonizer = (function($) {
     }
 
     function takePicture() {
-        _canvas.getContext("2d").drawImage(_video, 0, 0, canvas.width, canvas.height);
+        _canvas.getContext("2d").drawImage(_video, 0, 0, _canvas.width, _canvas.height);
+
+        var img = new Image();
+        img.onload = function() {
+            Pixastic.process(img, "sharpen", {
+                amount: $("#value-amount").val()
+            });
+            _canvas.drawImage(img);
+        }
+
+        img.src = _canvas.toDataURL("img/png");
     }
 
 })(jQuery, undefined);
