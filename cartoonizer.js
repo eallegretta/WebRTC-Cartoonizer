4ea134dac241e4e16f3d400354855f62ef320f2c
Paleta <epaleta@hotmail.com>
2012-08-30 23:53:18 -0300

    Added image buffer

--- cartoonizer.js
+++ cartoonizer.js
@@ -1,11 +1,11 @@
 var Cartoonizer = (function($) {
     var _video = document.getElementById("video");
-    var _canvas = document.getElementById("picture");
+    var _canvas = document.getElementById("pictureBuffer");
 
 
     $(document).ready(function() {
-        _canvas.width = 600;
-        _canvas.height = 600;
+        _canvas.width = 480;
+        _canvas.height = 360;
 
         captureVideo();
 
@@ -24,18 +24,12 @@ var Cartoonizer = (function($) {
 
     function takePicture() {
         _canvas.getContext("2d").drawImage(_video, 0, 0, _canvas.width, _canvas.height);
+        $("#picture")
 
-        var img = new Image();
-        img.width = _canvas.width;
-        img.height = _canvas.height;
-        img.onload = function() {
-            Pixastic.process(img, "sharpen", {
-                amount: 0.7
-            });
-            _canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
-        }
-
-        img.src = _canvas.toDataURL("img/png");
+            .load(function() {
+                $(this).pixastic("sharpen", { ammount: 0.7 });
+            })
+            .attr("src", _canvas.toDataURL("img/png"))
     }
 
 })(jQuery, undefined);
